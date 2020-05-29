252cbc97064c500b8128d95a3ada65b45463f15f
pspro0305 <38954487+pspro0305@users.noreply.github.com>
2020-05-29 18:31:02 +0530

    Add files via upload

---
+++ AngularCRUDPrakhar/Content/AngularAssets/Controller.js
@@ -0,0 +1,95 @@
+﻿app.controller("myCntrl", function ($scope, myService) {
+    $scope.divEmployee = false;
+    GetAllEmployee();
+    //To Get All Records 
+    function GetAllEmployee() {
+        debugger;
+        var getData = myService.getEmployees();
+        debugger;
+        getData.then(function (emp) {
+            $scope.employees = emp.data;
+        }, function () {
+            alert('Error in getting records');
+        });
+    }
+
+    $scope.editEmployee = function (employee) {
+        debugger;
+        var getData = myService.getEmployee(employee.Id);
+        getData.then(function (emp) {
+            $scope.employee = emp.data;
+            $scope.employeeId = employee.Id;
+            $scope.employeeName = employee.name;
+            $scope.employeeEmail = employee.email;
+            $scope.mobileno = employee.mobileno;
+            $scope.Action = "Update";
+            $scope.divEmployee = true;
+        },
+        function () {
+            alert('Error in getting records');
+        });
+    }
+    $scope.ResetForm = function () {
+        ClearFields();
+    }
+    $scope.AddUpdateEmployee = function () {
+        debugger;
+        if ($scope.employeeName == '' || $scope.employeeEmail == '' || $scope.mobileno=='') {
+            alert('Input Field is required');
+            return false;
+        }
+        var Employee = {
+            Name: $scope.employeeName,
+            Email: $scope.employeeEmail,
+            mobileno: $scope.mobileno
+        };
+        var getAction = $scope.Action;
+
+        if (getAction == "Update") {
+            //alert('Update')
+            Employee.Id = $scope.employeeId;
+            var getData = myService.updateEmp(Employee);
+            getData.then(function (msg) {
+                GetAllEmployee();
+                alert(msg.data);
+                $scope.divEmployee = false;
+            }, function () {
+                alert('Error in updating record');
+            });
+        } else {
+           // alert('add')
+            var getData = myService.AddEmp(Employee);
+            getData.then(function (msg) {
+                GetAllEmployee();
+                alert(msg.data);
+                $scope.divEmployee = false;
+            }, function () {
+                alert('Error in adding record');
+                console.log(Error);
+            });
+        }
+    }
+
+    $scope.AddEmployeeDiv = function () {
+        ClearFields();
+        $scope.Action = "Add";
+        $scope.divEmployee = true;
+    }
+
+    $scope.deleteEmployee = function (employee) {
+        var getData = myService.DeleteEmp(employee.Id);
+        getData.then(function (msg) {
+            GetAllEmployee();
+            alert('Employee Deleted');
+        }, function () {
+            alert('Error in Deleting Record');
+        });
+    }
+
+    function ClearFields() {
+        $scope.employeeId = "";
+        $scope.employeeName = "";
+        $scope.employeeEmail = "";
+        $scope.mobileno = "";
+    }
+});
---
+++ AngularCRUDPrakhar/Content/AngularAssets/Service.js
@@ -0,0 +1,57 @@
+﻿app.service("myService", function ($http) {
+
+    //get All Eployee
+    this.getEmployees = function () {
+        debugger;
+        return $http.get("../Home/GetAll");
+    };
+
+    // get Employee By Id
+    this.getEmployee = function (employeeID) {
+        var response = $http({
+            method: "post",
+            url: "../Home/getEmployeeByNo",
+            params: {
+                id: JSON.stringify(employeeID)
+            }
+        });
+        return response;
+    }
+
+    // Update Employee
+    this.updateEmp = function (employee) {
+        var response = $http({
+            method: "post",
+            url: "../Home/UpdateEmployee",
+            data: JSON.stringify(employee),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    // Add Employee
+    this.AddEmp = function (employee) {
+        // alert('addservice')
+        debugger
+        var response = $http({
+            method: "post",
+            url: "../Home/AddEmployee",
+            data: JSON.stringify(employee),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    //Delete Employee
+    this.DeleteEmp = function (employeeId) {
+       // alert(employeeId);
+        var response = $http({
+            method: "post",
+            url: "../Home/DeleteEmployee",
+            params: {
+                Id: JSON.stringify(employeeId)
+            }
+        });
+        return response;
+    }
+})
